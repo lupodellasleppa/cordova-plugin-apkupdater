3000092bf28f60ba6b1861a28ea0e4e56e352bea
Michael Jedich <michael.jedich@pm.me>
2021-05-30 19:36:02 +0200

    Testing out new restart mechanic - #15

--- src/android/PackageReplacedReceiver.java
+++ src/android/PackageReplacedReceiver.java
@@ -1,24 +1,41 @@
 package de.kolbasa.apkupdater;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.util.Log;
+import android.widget.Toast;
 
 public class PackageReplacedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        String packageName = context.getPackageName();
-        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-        String mainActivity = launchIntent.getComponent().getClassName();
+//             // Check if Android M or higher
+//             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                 Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+//                 cordova.getActivity().startActivity(intent);
+//             }
 
-        Intent activityIntent = new Intent();
-        activityIntent.setClassName(context, mainActivity);
-        activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if (intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
+            String packageName = context.getPackageName();
+            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+            String mainActivity = launchIntent.getComponent().getClassName();
 
-        context.startActivity(activityIntent);
+            Intent activityIntent = new Intent();
+            activityIntent.setClassName(context, mainActivity);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            context.startActivity(activityIntent);
+            if (context instanceof Activity) {
+                ((Activity) context).finish();
+            }
+            Runtime.getRuntime().exit(0);
+        }
+
     }
 
 }
